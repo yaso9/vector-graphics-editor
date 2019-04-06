1b03f19d8c72ceb88ad61b171fb1de702e61793b
Yahya Sohail <yahyasohail@hotmail.com>
2019-04-06 16:07:07 -0500

    Replace the proof of concept square with a square from Fabric

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,26 @@
+// App state
+let state = {
+    foreground: fabric.Color.fromHex('000000'),
+    background: [fabric.Color.fromHex('FFFFFF')]
+};
+
 // Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', () => {
     // Get the canvas and context
-    let canvas = document.getElementById('main-canvas');
-    let ctx = canvas.getContext('2d');
+    let canvasElement = document.getElementById('main-canvas');
 
     // Set canvas size
-    canvas.width = document.documentElement.clientWidth;
-    canvas.height = document.documentElement.clientHeight;
+    canvasElement.width = document.documentElement.clientWidth;
+    canvasElement.height = document.documentElement.clientHeight;
 
-    // Test the canvas
-    ctx.fillStyle = 'green';
-    ctx.fillRect(100, 100, 500, 500);
+    // Draw a rectangle with fabric
+    let canvas = new fabric.Canvas('main-canvas');
+    canvas.add(new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: state.foreground,
+        width: 100,
+        height: 100,
+        angle: 45
+    }))
 });
