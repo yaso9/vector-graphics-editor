252a137c19f3e212388e679a77753d45cc65143e
Yahya Sohail <yahyasohail@hotmail.com>
2019-04-06 17:12:02 -0500

    Add sidebar and tool to make a rectangle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,70 @@
-// App state
-let state = {
-    foreground: fabric.Color.fromHex('000000'),
-    background: [fabric.Color.fromHex('FFFFFF')]
-};
+function newRectangle() {
+    data.canvas.add(new fabric.Rect({
+        left: (data.canvasSize[0] - 100) / 2,
+        top: (data.canvasSize[1] - 100) / 2,
+        fill: data.foreground,
+        width: 100,
+        height: 100
+    }));
+}
 
 // Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', () => {
+    // App state
+    window.data = {
+        // ============
+        // CONSTS
+        // ============
+
+        // The canvas element (in the future)
+        canvas: {},
+
+        // Size of canvas
+        canvasSize: [
+            document.documentElement.clientWidth - 74,
+            document.documentElement.clientHeight
+        ],
+
+        // Tools
+        tools: [
+            {
+                name: 'Rectangle',
+                icon: 'rectangle',
+                action: newRectangle
+            }
+        ],
+
+        // ============
+        // END CONSTS
+        // ============
+
+        // Colors
+        foreground: fabric.Color.fromHex('000000'),
+        background: fabric.Color.fromHex('FFFFFF')
+    };
+
     // Get the canvas and context
     let canvasElement = document.getElementById('main-canvas');
 
-    // Set canvas size
-    canvasElement.width = document.documentElement.clientWidth;
-    canvasElement.height = document.documentElement.clientHeight;
+    // Set the canvas size
+    canvasElement.width = data.canvasSize[0];
+    canvasElement.height = data.canvasSize[1];
 
-    // Draw a rectangle with fabric
-    let canvas = new fabric.Canvas('main-canvas');
-    canvas.add(new fabric.Rect({
-        left: 100,
-        top: 100,
-        fill: state.foreground,
-        width: 100,
-        height: 100,
-        angle: 45
-    }))
+    // Create fabric canvas
+    data.canvas = new fabric.Canvas('main-canvas');
+
+    // Add tools to the DOM
+    data.tools.forEach((tool) => {
+        // Create DOM node
+        let el = document.createElement('div');
+
+        // Set properties
+        el.title = tool.name;
+        el.style.backgroundImage = 'url(\'/assets/img/' + tool.icon + '.png\')';
+        el.addEventListener('click', tool.action);
+        el.classList.add('tool');
+
+        // Add the node to the DOM
+        document.getElementById('sidebar').appendChild(el);
+    });
 });
